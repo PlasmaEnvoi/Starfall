d0ed35989361c3919b0c56a0f2d81e8d8eb18f76
Sophie Song <songsophie99@gmail.com>
2023-11-16 23:45:43 -0500

    Sound effects added to sound manager

--- TestStuff/SoundManager.gd
+++ TestStuff/SoundManager.gd
@@ -2,9 +2,39 @@ extends AudioStreamPlayer3D
 
 @export var attack_sounds: Array[AudioStream]
 
+var audioRNG = RandomNumberGenerator.new()
+
 func play_sound(type, strength):
 	
 	match type:
 		"Slash": 
-			stream = attack_sounds[0]
+			stream = attack_sounds[audioRNG.randi_range(0,1)]
+			play()
+		
+		"Charge":
+			stream = attack_sounds[2]
+			play()
+			
+		"Hurt":
+			stream = attack_sounds[3]
+			play()
+
+		"Parry":
+			stream = attack_sounds[4]
+			play()
+			
+		"Block":
+			stream = attack_sounds[5]
+			play()
+			
+		"Jump":
+			stream = attack_sounds[6]
+			play()
+			
+		"Land":
+			stream = attack_sounds[7]
+			play()
+		
+		"Dash":
+			stream = attack_sounds[8]
 			play()
